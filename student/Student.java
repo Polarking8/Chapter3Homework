51ef161d90be35846d8e35dfd66c5d3c09b00b8b
Polarking8 <57197175+Polarking8@users.noreply.github.com>
2021-11-26 17:29:20 -0700

    Made changes to get better score

--- student/Student.java
+++ student/Student.java
@@ -1,35 +1,50 @@
 package org.hillcrest.chapter3.student;
 public class Student {
-    private int TotalScore;
-    private int AverageScore;
-    public int NumberOfQuizzes = 1;
-    private int QuizScore;
+    private int totalScore;
+    private int averageScore;
+    private int numberOfQuizzes = 0;
+    private int quizScore;
     private String StudentName;
     /**
      * Creates an instance of a student
      * @param StudentName the name of the student
-     * @param FirstQuizScore the first quiz
      */
-        public Student(String StudentName, int FirstQuizScore){
+        public Student(String StudentName){
             this.StudentName = StudentName;
-            TotalScore = FirstQuizScore;
         }
     /**
-     *
+     * Adds scores to the total score and increases number of quizzes taken
      * @param QuizScore the score of quizzes being added
      */
-    public  void addScore(int QuizScore) {
-        TotalScore += QuizScore;
-        NumberOfQuizzes++;
+    public void addScore(int QuizScore) {
+        totalScore += QuizScore;
+        numberOfQuizzes = numberOfQuizzes + 1;
     }
+    /**
+     * Gets various variables
+     * @return getTotalScore - returns the total score
+     * @return getNumberOfQuizzes - returns number of quizzes taken
+     * @return getStudentName - returns the student name
+     */
     public double getTotalScore(){
-            return TotalScore;
+            return totalScore;
         }
         public double getNumberOfQuizzes(){
-            return NumberOfQuizzes;
+            return numberOfQuizzes;
         }
         public String getStudentName() {
             return StudentName;
         }
+    /**
+     * Calculates the quiz average by dividing total score by number of quizzes
+     * @return returns the average score
+     */
+    public double calculateQuizAverage() {
+        averageScore = (totalScore / numberOfQuizzes);
+        return averageScore;
+
     }
+}
+
+
 
--- student/StudentTester.java
+++ student/StudentTester.java
@@ -1,12 +1,14 @@
 package org.hillcrest.chapter3.student;
 
+import java.io.PrintStream;
+
 public class StudentTester {
         public static void main(String[] args) {
             String StudentName = "Kenobi";
-            Student student1 = new Student(StudentName, 93);
+            Student student1 = new Student(StudentName);
             student1.addScore(99);
             student1.addScore(95);
-            System.out.println("The current total score is now: " + student1.getTotalScore());
-            System.out.printf("The current average score is now: "+ student1.getTotalScore() / student1.getNumberOfQuizzes());
+            System.out.printf("The current total score is now: %.2f \n", student1.getTotalScore());
+            System.out.printf("The current average score is now: %.2f ", student1.calculateQuizAverage());
         }
 }
